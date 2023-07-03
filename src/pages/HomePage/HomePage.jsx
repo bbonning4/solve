879a88a869359bcc3ff294ae11627161a6855fec
bbonning4 <bbonning42@gmail.com>
2023-07-03 11:08:13 -0500

    deploy

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -7,25 +7,31 @@ export default function HomePage() {
   const [previewImage, setPreviewImage] = useState("");
   const [result, setResult] = useState("");
   const [mathpix, setMathpix] = useState("");
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
-  
 
   async function handleImageInput(evt) {
     const file = evt.target.files[0];
-    
+
     const reader = new FileReader();
     reader.onload = async (e) => {
       setPreviewImage(e.target.result);
+      setLoading(true);
+
       const aiResponse = await aiAPI.processImage({ image: e.target.result });
-      setMathpix(`${aiResponse.tex}`)
+      setMathpix(`${aiResponse.tex}`);
       setResult(`${aiResponse.result}`);
+
+      setLoading(false);
     };
     reader.readAsDataURL(file);
   }
 
   function handlePostClick() {
-    navigate('/posts/new', { state: { base64: previewImage, mathpix: mathpix, result: result } });
-  } 
+    navigate("/posts/new", {
+      state: { base64: previewImage, mathpix: mathpix, result: result },
+    });
+  }
 
   return (
     <>
@@ -42,14 +48,21 @@ export default function HomePage() {
         <input type="file" accept="image/*" onChange={handleImageInput} />
         {previewImage && <img src={previewImage} alt="image preview" />}
       </form>
-      {result && (
-        <div>
-          <MathJax>Answer: {result}</MathJax>
+      {loading ? (
+        <span className="loading loading-infinity loading-lg"></span>
+      ) : (
+        result && (
           <div>
-            <p>Don't like this answer?</p>
-            <button className="btn bg-primary" onClick={handlePostClick} >Post</button> your image for others to answer!
+            <MathJax>Answer: {result}</MathJax>
+            <div>
+              <p>Don't like this answer?</p>
+              <button className="btn bg-primary" onClick={handlePostClick}>
+                Post
+              </button>{" "}
+              your image for others to answer!
+            </div>
           </div>
-        </div>
+        )
       )}
     </>
   );
